ff579ae98f27444ae1dc9954880ee2a5d5b80330
devhnry <taiwo@systemspecscomng.onmicrosoft.com>
2024-08-20 16:54:45 +0100

    fix: Resolves Email Template not Processing Issue

--- src/main/java/org/henry/onlinebankingsystemp/service/EmailService.java
+++ src/main/java/org/henry/onlinebankingsystemp/service/EmailService.java
@@ -1,9 +1,12 @@
 package org.henry.onlinebankingsystemp.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.AddressException;
+import org.springframework.scheduling.annotation.Async;
 import org.thymeleaf.context.Context;
 
 import javax.mail.*;
 
 public interface EmailService {
-    void sendEmail(String toEmail, String subject, Context context, String template);
+    void sendEmail(String toEmail, String subject, Context context);
 }
--- src/main/java/org/henry/onlinebankingsystemp/service/impl/EmailServiceImpl.java
+++ src/main/java/org/henry/onlinebankingsystemp/service/impl/EmailServiceImpl.java
@@ -9,32 +9,28 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
-
-
-import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 @Service @Slf4j
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
-    private final TemplateEngine templateEngine;
     private final JavaMailSender mailSender;
+    private final SpringTemplateEngine springTemplateEngine;
 
     @Value("${spring.mail.username}")
     private String SENDER_EMAIL;
 
-
     @Async("customEmailExecutor")/* Send Email Asynchronously */
     @Override
-    public void sendEmail(String toEmail, String subject, Context context, String template) {
+    public void sendEmail(String toEmail, String subject, Context context) {
         int maxRetries = 3;
         int retryCount = 0;
         long retryDelay = 2000; // 2 second
 
-        String htmlContent = templateEngine.process(template, context);
-        //noinspection ConstantValue
+        final String htmlContent = springTemplateEngine.process("onboardTemplate", context);
+
         while (retryCount < maxRetries) {
             try {
                 MimeMessage message = mailSender.createMimeMessage();
@@ -43,18 +39,17 @@ public class EmailServiceImpl implements EmailService {
                 helper.setTo(toEmail);
                 helper.setSubject(subject);
                 helper.setText(htmlContent, true);
-
                 mailSender.send(message);
 
                 /* If successful, break out of the retry loop */
-                System.out.println("Email sent successfully");
+                log.info("Email sent successfully");
                 return;
             } catch (Exception e) {
                 retryCount++;
-                System.out.println("Attempt " + retryCount + " failed. Error: " + e.getMessage());
+                log.debug("Attempt {} failed. Error: {}", retryCount, e.getMessage());
 
                 if (retryCount >= maxRetries) {
-                    System.out.println("All retry attempts failed. Giving up.");
+                    log.debug("All retry attempts failed. Giving up.");
                     throw new RuntimeException("Failed to send email after " + maxRetries + " attempts", e);
                 }
 
